1e602db2cb47c4fed525e956dee592a93447b219
Vinicius Lourenço Ferreira <lourencovinicius00@gmail.com>
2022-11-07 23:07:19 -0300

    Criação do carrinho de produtos

--- cart.js
+++ cart.js
@@ -6,24 +6,24 @@ let itens = [];
 
 async function renderizarItens() {         
 
-    let html = '';
+    let html = '';    
 
     if (itens) {
-        itens.forEach(item => {
+        itens.forEach(item => {            
     
             let htmlSegment = 
                 `<div class="cart__produto_top">
                     <img class="cart__produto_foto" src="images/image-product-1.jpg" alt="">  
                     <div class="cart__produto_info">
-                    <p class="cart__produto_info_descricao">${item[0].nome}</p>
+                    <p class="cart__produto_info_descricao">${item[1].nome}</p>
                     <div class="cart__produto_info_valores">
-                        <span class="cart__produto_info_valor">$${item[1].valor_item.toFixed(2)}</span> 
+                        <span class="cart__produto_info_valor">$${item[2].valor_item.toFixed(2)}</span> 
                         <span>x</span>
-                        <span class="cart__produto_info_qtd">${item[2].qtd_item}</span>
-                        <span class="cart__produto_info_valortotal">$${item[3].valor_totalitem.toFixed(2)}</span>
+                        <span class="cart__produto_info_qtd">${item[3].qtd_item}</span>
+                        <span class="cart__produto_info_valortotal">$${item[4].valor_totalitem.toFixed(2)}</span>
                     </div>              
                     </div>
-                    <img class="cart__produto_delete" src="images/icon-delete.svg" alt="">
+                    <img class="cart__produto_delete" id="${item[0].id}" src="images/icon-delete.svg" alt="">
                 </div>`;                
                 
             html += htmlSegment;                               
@@ -36,15 +36,20 @@ async function renderizarItens() {
 
 }                        
 
+let idItem = 0;
+
 function addItem() {
 
     const nomeItem = document.querySelector('.conteudo__info_titulo').innerHTML;
     const valorItem =  parseFloat(document.querySelector('.conteudo__info_preco_atual').innerHTML.replace('$',''));
     const qtdItem = parseFloat(document.querySelector('.conteudo__info_botao_qtd').innerHTML);
-    const valorTotalItem = valorItem * qtdItem;
+    const valorTotalItem = valorItem * qtdItem; 
+    
+    idItem += 1;
 
     itens.push(
         [
+            {'id': idItem},
             {'nome': nomeItem},
             {'valor_item': valorItem},
             {'qtd_item': qtdItem}, 
@@ -62,10 +67,23 @@ function addItem() {
 }
 
 botaoAdicionarAoCarrinho.onclick = function () {
-    addItem();
-    console.log(itens)
+    addItem();    
 }
 
-function removerItem () {
-    
+function removerItem (id) {
+
+    var index = itens.findIndex(p => p.id == id);
+
+    // const index = itens.indexOf(id);
+    if (index > -1) { // only splice array when item is found
+      array.splice(index, 1); // 2nd parameter means remove one item only
+    }        
+}
+
+const botaoRemoverDoCarrinho = document.querySelector('.cart__produto_delete')
+
+botaoRemoverDoCarrinho.onclick = function () {
+    removerItem(botaoRemoverDoCarrinho.id);
+
+    renderizarItens();
 }
